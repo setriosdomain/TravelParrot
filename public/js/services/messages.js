6c24452d1e9bc7de19eab4f08c7a9300dcb37aa5
Nicolas Larrea <nicolaslarrea@gmail.com>
2013-12-23 12:43:17 -0500

    added participants to events

--- public/js/services/messages.js
+++ public/js/services/messages.js
@@ -1,10 +1,5 @@
 //Messages service used for articles REST endpoint
 angular.module('mean.messages').factory("Messages", ['$resource', function($resource) {
-    return $resource('messages/:messageId', {
-        messageId: '@_id'
-    }, {
-        update: {
-            method: 'PUT'
-        }
-    });
+    return $resource('messages/:messageId', {messageId: '@_id'},
+        {update: {method: 'PUT'}});
 }]);
